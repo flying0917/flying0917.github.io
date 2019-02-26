7934fd73fa2ac069b2371fcbef5e6e648b04765e
flying0917 <1502424388@qq.com>
2019-02-27 00:37:43 +0800

    sss
    sxx

---
+++ my-ui/js/cui-pullrefresh.js
@@ -0,0 +1,207 @@
+//下拉刷新插件  @todo 王榕 这个可能复杂点 嘻嘻
+(function(){
+    "use strict";
+    var cuiPullRefresh=function(param,fn)
+    {
+        //是否开始
+        var start=false,
+        //从手指触到屏幕时的Y坐标
+        startPageY=0,
+        //从手指触到屏幕时的Y坐标
+        startPageX=0,
+        //刷新头的高度
+        refreshHeaderHeight=50,
+        //容器里面的html字符串
+        contentHtml="",
+        //是否在刷新
+        isRefreshing=false,
+        //cuiPUllRefresh
+        that=this,
+        refresContentDom,
+        //要拖动的容器
+        wrapContentDom,
+        //刷新的头
+        headerContentDom,
+        //刷新文本提示
+        headerTextDom,
+        //刷新提示图标
+        headerImgDom,
+            //手指滑动的Y长度
+        offsetTop,
+        //手指滑动的X长度
+        offsetLeft;
+        //下拉的距离的触发点
+        that.triggerDistance=parseInt(param.triggerDistance)?parseInt(param.triggerDistance):200;
+        //下拉时
+        that.contentdownIcon=param.contentdownIcon?param.contentdownIcon:"http://static.oschina.net/uploads/img/201409/26074001_bzCh.gif";
+        that.contentdown=param.contentdown?param.contentdown:"下拉刷新";
+        //放开时
+        that.contentoverIcon=param.contentoverIcon?param.contentoverIcon:"http://static.oschina.net/uploads/img/201409/26074001_bzCh.gif";
+        that.contentover=param.contentover?param.contentover:"松开刷新";
+        //刷新时
+        that.contentrefreshIcon=param.contentrefreshIcon?param.contentrefreshIcon:"http://static.oschina.net/uploads/img/201409/26074001_bzCh.gif";
+        that.contentrefresh=param.contentrefresh?param.contentrefresh:"刷新中";
+        //完成刷新时
+        that.contentdoneIcon="http://static.oschina.net/uploads/img/201409/26074001_bzCh.gif";
+        that.contentdone=param.contentdone?param.contentdone:"完成刷新";
+        //触发刷新时的回调
+        that.callback=(fn&&typeof fn==="function")?fn:(function(){});
+        if(!param.domStr)
+        {
+            console.log("缺少容器选择字符串");
+            return false;
+        }
+        //获取容器
+        if(typeof param.domStr==="string")
+        {
+            refresContentDom=param.domStr?document.querySelector(param.domStr):document.querySelector(".cui-refresh-content");
+        }
+        else if(typeof param.domStr==="object")
+        {
+            refresContentDom=param.domStr;
+        }
+        if(!refresContentDom)
+        {
+            console.log("缺少容器选择字符串");
+            return false;
+        }
+        //添加类的公用函数
+        that.addClass=function(dom,clsName)
+        {
+            var nowClsName=dom.className;
+            if(nowClsName.indexOf(clsName)===-1)
+            {
+                dom.className=nowClsName+" "+clsName;
+            }
+            return false;
+
+        };
+        //渲染html
+        var renderContent=function()
+        {
+            var temHtml="";
+            that.addClass(refresContentDom,"cui-refresh-content");
+            contentHtml=refresContentDom.innerHTML;
+            temHtml='<div class="cui-refresh-wrap" style="-webkit-transform: translateY(0px);\n' +
+                '\t\t\t-moz-transform: translateY(0px);\n' +
+                '\t\t\t-ms-transform: translateY(0px);\n' +
+                '\t\t\t-o-transform: translateY(0px);\n' +
+                '\t\t\ttransform: translateY(0px);">' +
+                '<div class="cui-refresh-header" style="line-height:'+refreshHeaderHeight+'px;height:'+refreshHeaderHeight+'px;top:-'+refreshHeaderHeight+'px">' +
+                '<img src="'+that.contentdownIcon+'">'+
+                '<span>'+that.contentdown+'</span>' +
+                '</div>'
+                +contentHtml+
+                '</div>';
+            refresContentDom.innerHTML=temHtml;
+            //要拖动的容器
+            wrapContentDom=refresContentDom.children[0];
+            //刷新的头
+            headerContentDom=wrapContentDom.children[0];
+            //刷新文本提示
+            headerTextDom=headerContentDom.children[1];
+            //刷新提示图标
+            headerImgDom=headerContentDom.children[0];
+        };
+        //绑定事件
+        var bindEvent=function()
+        {
+            wrapContentDom.addEventListener("touchstart",function(e)
+            {
+                if(!isRefreshing&&refresContentDom.scrollTop===0)//当容器的滚动条滚动距离为0和不是在刷新状态时调用
+                {
+                    start=true;//开始
+                    startPageY=e.changedTouches[0].pageY;//从手指触到屏幕时的Y坐标的赋值
+                    startPageX=e.changedTouches[0].pageX;//从手指触到屏幕时的X坐标的赋值
+                }
+            });
+            wrapContentDom.addEventListener("touchmove",function(e)
+            {
+                if(!isRefreshing&&refresContentDom.scrollTop===0)//当容器的滚动条滚动距离为0和不是在刷新状态时调用
+                {
+                    //手指滑动的Y长度
+                    offsetTop=e.changedTouches[0].pageY-startPageY;
+                    //手指滑动的X长度
+                    offsetLeft=e.changedTouches[0].pageX-startPageX;
+
+                    if(start&&offsetTop>0&&Math.abs(offsetTop/offsetLeft)>1.5)//下划角度超过45度时不干活
+                    {
+                        //下拉的动画
+                        wrapContentDom.style.transform=wrapContentDom.style.webkitTransform="translateY("+offsetTop+"px)";
+                        //是否大于下拉的距离的触发点
+                        if(offsetTop>that.triggerDistance)
+                        {
+                            headerTextDom.innerText=that.contentover;
+                            headerImgDom.src=that.contentoverIcon;
+                        }
+                        else
+                        {
+                            headerTextDom.innerText=that.contentdown;
+                            headerImgDom.src=that.contentdownIcon;
+                        }
+                    }
+                }
+            });
+            wrapContentDom.addEventListener("touchend",function(e)
+            {
+                if(!isRefreshing&&start&&offsetTop>-refreshHeaderHeight)
+                {
+                    start=false;
+                    wrapContentDom.style.transition=wrapContentDom.style.webkitTransition="all .5s";
+                    //是否大于下拉的距离的触发点
+                    if(offsetTop>that.triggerDistance)
+                    {
+                        //显示刷新中的动画
+                        wrapContentDom.style.transform=wrapContentDom.style.webkitTransform="translateY("+refreshHeaderHeight+"px)";
+                        setTimeout(function(){
+                            wrapContentDom.style.transition=wrapContentDom.style.webkitTransition="";
+                            //改变状态（正在刷新）
+                            isRefreshing=true;
+                            headerTextDom.innerText=that.contentrefresh;
+                            headerImgDom.src=that.contentrefreshIcon;
+                            //开始回调
+                            that.callback();
+                        },500)
+
+                    }
+                    else
+                    {
+                        //还有到触发点，收起刷新动画
+                        that.closeRefresh();
+                    }
+                }
+            });
+        };
+        //收起刷新的动画
+        that.closeRefresh=function()
+        {
+            //改变状态（不在刷新）
+            isRefreshing=false;
+            wrapContentDom.style.transition=wrapContentDom.style.webkitTransition="all .5s";
+            wrapContentDom.style.transform=wrapContentDom.style.webkitTransform="translateY(0px)";
+            setTimeout(function(){
+                //清除动画
+                wrapContentDom.style.transition=wrapContentDom.style.webkitTransition="";
+            },500)
+        };
+        //完成刷新回调
+        that.refreshDone=function()
+        {
+            headerTextDom.innerText=that.contentdone;
+            headerImgDom.src=that.contentdoneIcon;
+            setTimeout(function(){
+                that.closeRefresh();
+            },500);
+        };
+        //初始化
+        var init=function()
+        {
+            //渲染html
+            renderContent();
+            //绑定事件
+            bindEvent();
+        };
+        init();
+    }
+    window.cuiPullRefresh=cuiPullRefresh
+})();
---
+++ my-ui/js/cui-swiper.js
@@ -0,0 +1,426 @@
+//轮播插件
+(function(){
+    "use strict";
+    var cuiSwiper=function(param,fn)
+    {
+        //默认值
+        var defaults={
+                domStr:"",
+                triggerDistance:100,//滑动距离触发点
+                width:0,
+                height:0,
+                noop:true,//是否可循环 bool 默认 true
+                direction: 'horizontal',//（默认）水平 horizontal， 垂直 vertical   string
+                autoplay:0,//自动播放 默认0 不自动播放
+                speed:0.5,//滑动速度以秒为单位
+                pagination:"",//分页容器选择字符
+                change:function(a){
+                    console.log("change===="+a)
+                },//切换轮播图回调事件
+                beforeChange:function(a){
+                    console.log("beforChange==="+a);
+                }//改变之前要干的事
+            },
+            that=this,
+            contentDom=null,//容器
+            wrapDom=null,//移动的容器
+            now=0,
+            startX=0,
+            moveX=0,
+            startY=0,
+            moveY=0,
+            nowX=0,
+            nowY=0,
+            count=0,//slide数量
+            index=0,//目前展示的slide索引
+            ismoving=false,
+            beforeNoopDom=null,
+            afterNoopDom=null;
+        //属性替换
+        that.extend=function(def,param){
+            for(var x in param)
+            {
+                if(def.hasOwnProperty(x))
+                {
+                    def[x]=param[x];
+                }
+            }
+            return def;
+        };
+        //添加类的公用函数
+        that.addClass=function(dom,clsName)
+        {
+            var nowClsName=dom.className;
+            if(nowClsName.indexOf(clsName)===-1)
+            {
+                dom.className=nowClsName.trim()+" "+clsName;
+            }
+            return false;
+
+        };
+        //删除类的公用函数
+        that.removeClass=function(dom,clsName)
+        {
+            var reg=new RegExp(clsName,"g");
+            if(dom.length)
+            {
+                for(var x=0;x<dom.length;x++)
+                {
+                    var itemClsName=dom[x].className;
+                        itemClsName=itemClsName.replace(reg,"");
+                    dom[x].className=itemClsName;
+                }
+                return false;
+            }
+            else
+            {
+                var nowClsName=dom.className;
+                nowClsName=nowClsName.replace(reg,"");
+                dom.className=nowClsName;
+                return false;
+            }
+
+        };
+        defaults=that.extend(defaults,param);
+
+        if(defaults.pagination)
+        {
+            var paginationDom=null;
+        }
+        //函数绑定
+        that.bindEvent=function()
+        {
+            contentDom.addEventListener("touchstart",function(e)
+            {
+                startX=e.changedTouches[0].pageX;
+                startY=e.changedTouches[0].pageY;
+
+                moveX=0;
+                moveY=0;
+                wrapDom.style.transition=wrapDom.style.webkitTransition="";
+            });
+            contentDom.addEventListener("touchmove",function(e)
+            {
+                if(ismoving===false)
+                {
+                    moveX=e.changedTouches[0].pageX-startX;
+                    moveY=e.changedTouches[0].pageY-startY;
+                    if(defaults.direction==="vertical")//垂直
+                    {
+                        if(Math.abs(moveY/moveX)>1.5)//垂直滑动角度超过45度不干活
+                        {
+                            if(!defaults.noop)
+                            {
+                                if(index===0&&moveY>0)
+                                {
+                                    return false;
+                                }
+                                if(index===(count-1)&&moveY<0)
+                                {
+                                    return false;
+                                }
+                            }
+                            wrapDom.style.transition=wrapDom.style.webkitTransition="";
+                            wrapDom.style.marginTop=(nowY+moveY)+"px";
+                        }
+                    }
+                    else//水平
+                    {
+                        if(Math.abs(moveX/moveY)>1.5)//水平滑动角度超过45度不干活
+                        {
+                            if(!defaults.noop)
+                            {
+                                if(index===0&&moveX>0)
+                                {
+                                    return false;
+                                }
+                                if(index===(count-1)&&moveX<0)
+                                {
+                                    return false;
+                                }
+                            }
+                            wrapDom.style.transition=wrapDom.style.webkitTransition="";
+                            wrapDom.style.marginLeft=(nowX+moveX)+"px";
+                        }
+                    }
+                }
+            });
+            contentDom.addEventListener("touchend",function(e)
+            {
+                var direct=defaults.direction==="vertical"?moveY:moveX;
+                //开始移动
+                that.move(direct);
+            });
+        };
+        //移动 (direct >0 )后退  (direct < 0 )前进
+        that.move=function(direct)
+        {
+            if(defaults.direction==="vertical")//垂直
+            {
+                if(!defaults.noop)
+                {
+                    if(index===0&&direct>0)
+                    {
+                        return false;
+                    }
+                    if(index===(count-1)&&direct<0)
+                    {
+                        return false;
+                    }
+                }
+                if(ismoving===false)
+                {
+                    if(Math.abs(direct)>defaults.triggerDistance)
+                    {
+                        direct>0?(now+=parseInt(defaults.height)):(now-=parseInt(defaults.height));
+                        ismoving=true;
+                        //触发改变之前要干的事
+                        defaults.beforeChange(index);
+                        wrapDom.style.transition=wrapDom.style.webkitTransition="all "+defaults.speed+"s";
+                        wrapDom.style.marginTop=now+"px";
+                        setTimeout(function(){
+                            ismoving=false;
+                            nowY=parseInt(wrapDom.style.marginTop);
+                            wrapDom.style.transition=wrapDom.style.webkitTransition="";
+                            direct>0?(index--):(index++);
+                            that.removeClass(wrapDom.children,"cui-swiper-active");
+                            wrapDom.children[index]?that.addClass(wrapDom.children[index],"cui-swiper-active"):"";
+                            if(defaults.noop&&index===(count-1)&&direct<0)
+                            {
+                                that.preInitStatus();
+                            }
+                            if(defaults.noop&&index===0&&direct>0)
+                            {
+                                that.nextInitStatus();
+                            }
+
+                            //分页状态
+                            if(defaults.pagination&&paginationDom)
+                            {
+                                that.removeClass(paginationDom.children,"cui-swiper-pagination-acitve");
+                                that.addClass(paginationDom.children[index],"cui-swiper-pagination-acitve");
+                            }
+                            //触发切换轮播图回调事件
+                            var nowIndex=defaults.noop?index-1:index;
+                            defaults.change(nowIndex);
+
+                        },parseFloat(defaults.speed)*1000)
+                    }
+                    else //没超过触发点 还原动画
+                    {
+                        ismoving=true;
+                        wrapDom.style.transition=wrapDom.style.webkitTransition="all "+defaults.speed+"s";
+                        wrapDom.style.marginTop=nowY+"px";
+                        setTimeout(function(){
+                            ismoving=false;
+                        },parseFloat(defaults.speed)*1000)
+                    }
+                }
+            }
+            else
+            {
+                if(!defaults.noop)
+                {
+                    if(index===0&&direct>0)
+                    {
+                        return false;
+                    }
+                    if(index===(count-1)&&direct<0)
+                    {
+                        return false;
+                    }
+                }
+                if(ismoving===false)
+                {
+                    if(Math.abs(direct)>defaults.triggerDistance)
+                    {
+                        direct>0?(now+=parseInt(defaults.width)):(now-=parseInt(defaults.width));
+                        ismoving=true;
+                        //触发改变之前要干的事
+                        defaults.beforeChange(index);
+                        wrapDom.style.transition=wrapDom.style.webkitTransition="all "+defaults.speed+"s";
+                        wrapDom.style.marginLeft=now+"px";
+                        setTimeout(function(){
+                            ismoving=false;
+                            nowX=parseInt(wrapDom.style.marginLeft);
+                            wrapDom.style.transition=wrapDom.style.webkitTransition="";
+                            direct>0?(index--):(index++);
+                            that.removeClass(wrapDom.children,"cui-swiper-active");
+                            wrapDom.children[index]?that.addClass(wrapDom.children[index],"cui-swiper-active"):"";
+                            if(defaults.noop&&index===(count-1)&&direct<0)
+                            {
+                                that.preInitStatus();
+                            }
+                            if(defaults.noop&&index===0&&direct>0)
+                            {
+                                that.nextInitStatus();
+                            }
+
+                            //分页状态
+                            if(defaults.pagination&&paginationDom)
+                            {
+                                that.removeClass(paginationDom.children,"cui-swiper-pagination-acitve");
+                                that.addClass(paginationDom.children[index],"cui-swiper-pagination-acitve");
+                            }
+                            //触发切换轮播图回调事件
+                            var nowIndex=defaults.noop?index-1:index;
+                            defaults.change(nowIndex);
+                        },500)
+                    }
+                    else //没超过触发点 还原动画
+                    {
+                        ismoving=true;
+                        wrapDom.style.transition=wrapDom.style.webkitTransition="all "+defaults.speed+"s";
+                        wrapDom.style.marginLeft=nowX+"px";
+                        setTimeout(function(){
+                            ismoving=false;
+                        },500)
+                    }
+                }
+            }
+        };
+        //开始的初始状态（可循环时）
+        that.preInitStatus=function()
+        {
+            if(defaults.direction==="vertical")
+            {
+                wrapDom.style.marginTop="-"+defaults.height+"px";
+                now=nowY=parseInt("-"+defaults.height);
+                console.log(232)
+            }
+            else
+            {
+                wrapDom.style.marginLeft="-"+defaults.width+"px";
+                now=nowX=parseInt("-"+defaults.width);
+            }
+            index=1;
+            that.removeClass(wrapDom.children,"cui-swiper-active");
+            wrapDom.children[index]?that.addClass(wrapDom.children[index],"cui-swiper-active"):"";
+        };
+        //结束的初始状态（可循环时）
+        that.nextInitStatus=function()
+        {
+            if(defaults.direction==="vertical")
+            {
+                wrapDom.style.marginTop="-"+(defaults.height*(count-2))+"px";
+                now=nowY=parseInt("-"+(defaults.height*(count-2)));
+            }
+            else
+            {
+                wrapDom.style.marginLeft="-"+(defaults.width*(count-2))+"px";
+                now=nowX=parseInt("-"+(defaults.width*(count-2)));
+            }
+            index=count-2;
+            that.removeClass(wrapDom.children,"cui-swiper-active");
+            wrapDom.children[index]?that.addClass(wrapDom.children[index],"cui-swiper-active"):"";
+        };
+
+        //定时器
+        that.setAutoplay=function()
+        {
+            var autoplayInterval=null;
+            if(defaults.autoplay)
+            {
+                autoplayInterval=setInterval(function(){
+                    if(!defaults.noop&&index===count-1)
+                    {
+                        clearInterval(autoplayInterval);
+                    }
+                    that.move(parseInt("-"+(defaults.triggerDistance+1)));
+                },parseInt(defaults.autoplay));
+            }
+        };
+
+        //初始化
+        that.init=function()
+        {
+            //获取容器
+            contentDom=document.querySelector(defaults.domStr);
+            if(defaults.direction==="vertical")
+            {
+                that.addClass(contentDom,"cui-swiper-content-v");
+            }
+            wrapDom=contentDom.children[0];
+            //slider的数量
+            count=wrapDom.children.length;
+            //获取容器的宽度
+            if(!defaults.width)
+            {
+                defaults.width=contentDom.offsetWidth;
+            }
+            else
+            {
+                contentDom.style.width=defaults.width+"px";
+            }
+            //获取容器的高度
+            if(!defaults.height)
+            {
+                defaults.height=contentDom.offsetHeight;
+            }
+            else
+            {
+                contentDom.style.height=defaults.height+"px";
+            }
+            //是否可以循环
+            if(defaults.noop)
+            {
+                wrapDom.appendChild(wrapDom.children[0].cloneNode(true));
+                wrapDom.insertBefore(wrapDom.children[count-1].cloneNode(true),wrapDom.children[0]);
+                afterNoopDom=wrapDom.children[0];
+                beforeNoopDom=wrapDom.children[count-1];
+                that.preInitStatus();
+                count=wrapDom.children.length;
+                that.addClass(wrapDom.children[1],"cui-swiper-active");
+            }
+            else
+            {
+                that.addClass(wrapDom.children[0],"cui-swiper-active")
+            }
+
+            //分页
+            if(defaults.pagination)
+            {
+                var paginationHtmlStr="";
+                if(paginationDom=document.querySelector(defaults.pagination))
+                {
+                    for(var y=0;y<count;y++)
+                    {
+                        if(defaults.noop)
+                        {
+                            if(y===0)
+                            {
+                                paginationHtmlStr+="<span style='display:none;'></span>";
+                                continue;
+                            }
+                            else if(y===1)
+                            {
+                                paginationHtmlStr+="<span class='cui-swiper-pagination-acitve'></span>";
+                                continue;
+                            }
+                            else if(y===count-1)
+                            {
+                                paginationHtmlStr+="<span style='display:none;'></span>";
+                                continue;
+                            }
+                        }
+                        else
+                        {
+                            if(y===0)
+                            {
+                                paginationHtmlStr+="<span class='cui-swiper-pagination-acitve'></span>";
+                                continue;
+                            }
+                        }
+                        paginationHtmlStr+="<span></span>";
+                    }
+                    paginationDom.innerHTML=paginationHtmlStr;
+                }
+            }
+            that.bindEvent();
+        };
+        that.init();
+        that.setAutoplay();
+
+    };
+    window.cuiSwiper=cuiSwiper
+})();
